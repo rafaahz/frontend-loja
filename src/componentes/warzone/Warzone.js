ca1b68cf1239e74e1edee49480657b82a6908e48
unknown <rafaelvinicius_10@hotmail.com>
2020-04-02 14:11:43 -0300

    ajuste na tabela

--- src/componentes/warzone/Warzone.js
+++ src/componentes/warzone/Warzone.js
@@ -23,7 +23,9 @@ export default function Warzone() {
                 let obj = {
                     player: nickname,
                     level: res.data.level,
-                    data: res.data.lifetime.mode.br.properties
+                    data: res.data.lifetime.mode.br.properties,
+                    saque: res.data.lifetime.mode.br_dmz.properties,
+                    all: res.data.lifetime.mode.br_all.properties,
                 };
                 objetos.push(obj);
                 if(objetos.length === listUser.length){
@@ -43,47 +45,76 @@ export default function Warzone() {
         {
             lista ? lista.map( (v,i)=>{
                 return (
-                    <table key={i} className="table table-striped table-hover col-12 ml-auto mr-auto mb-5 border">
-                    <tbody>
+                    <table key={i} className="table table-dark table-striped table-hover col-12 ml-auto mr-auto mb-5 border">
+
+                    <thead style={{borderBottom: "2px solid white"}} >
+                        <tr>
+                            <td style={{width: 25+"%"}}>INFORMAÇÃO</td>
+                            <td className="text-center" style={{width: 25+"%"}}>BATTLE ROYAL</td>
+                            <td className="text-center" style={{width: 25+"%"}}>SAQUE</td>
+                            <td className="text-center" style={{width: 25+"%"}}>TOTAL</td>
+                        </tr>
                         <tr>
                             <td>PLAYER</td>
-                            <td className="text-center" style={{width: 35+"%"}}>{v.player}</td>
+                            <td className="text-center" style={{width: 25+"%"}}>{v.player}</td>
+                            <td className="text-center" style={{width: 25+"%"}}>{v.player}</td>
+                            <td className="text-center" style={{width: 25+"%"}}>{v.player}</td>
                         </tr>
                         <tr>
                             <td>LEVEL</td>
                             <td className="text-center">{v.level}</td>
+                            <td className="text-center">{v.level}</td>
+                            <td className="text-center">{v.level}</td>
                         </tr>
+                    </thead>
+                    <tbody>
                         <tr>
                             <td>ABATES</td>
                             <td className="text-center">{v.data.kills}</td>
+                            <td className="text-center">{v.saque.kills}</td>
+                            <td className="text-center">{v.all.kills}</td>
                         </tr>
                         <tr>
                             <td>MORTES</td>
                             <td className="text-center">{v.data.deaths}</td>
+                            <td className="text-center">{v.saque.deaths}</td>
+                            <td className="text-center">{v.all.deaths}</td>
                         </tr>
                         <tr>
                             <td>DERRUBADO</td>
                             <td className="text-center">{v.data.downs}</td>
+                            <td className="text-center">{v.saque.downs}</td>
+                            <td className="text-center">{v.all.downs}</td>
                         </tr>
                         <tr>
                             <td>K/D RATIO</td>
                             <td className="text-center">{parseFloat(v.data.kdRatio).toFixed(3)}</td>
+                            <td className="text-center">{parseFloat(v.saque.kdRatio).toFixed(3)}</td>
+                            <td className="text-center">{parseFloat(v.all.kdRatio).toFixed(3)}</td>
                         </tr>
                         <tr>
                             <td>CONTRATOS</td>
                             <td className="text-center">{v.data.contracts}</td>
+                            <td className="text-center">{v.saque.contracts}</td>
+                            <td className="text-center">{v.all.contracts}</td>
                         </tr>
                         <tr>
                             <td>QNT. JOGOS</td>
                             <td className="text-center">{v.data.gamesPlayed}</td>
-                        </tr>
-                        <tr>
-                            <td>TEMPO DE JOGO (HR)</td>
-                            <td className="text-center">{((parseInt(v.data.timePlayed)/60)/60).toFixed(0)}</td>
+                            <td className="text-center">{v.saque.gamesPlayed}</td>
+                            <td className="text-center">{v.all.gamesPlayed}</td>
                         </tr>
                         <tr>
                             <td>QNT. VITORIAS</td>
                             <td className="text-center">{v.data.wins}</td>
+                            <td className="text-center">{v.saque.wins}</td>
+                            <td className="text-center">{v.all.wins}</td>
+                        </tr>
+                        <tr>
+                            <td>TEMPO DE JOGO (HR)</td>
+                            <td className="text-center">{((parseInt(v.data.timePlayed)/60)/60).toFixed(0)}</td>
+                            <td className="text-center">{((parseInt(v.saque.timePlayed)/60)/60).toFixed(0)}</td>
+                            <td className="text-center">{((parseInt(v.all.timePlayed)/60)/60).toFixed(0)}</td>
                         </tr>
 
                     </tbody>
